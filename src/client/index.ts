f28f45e0053bfb0a614db638ff1082e20748ff66
headpatmonster <krysppy@gmail.com>
2022-08-17 03:08:56 -0500

    Remove index.ts

--- src/client/index.ts
+++
@@ -1,8 +0,0 @@
-/*
-  Start by having a "waiting for lobby" text on the screen
-  Then, when the lobby has been initialized, use animation to load up the lobby which just displays a list of players in the lobby
-  When the game starts, use the animation to load up the game
-  When the game ends, use the animation to close out the game and bring back up the lobby container
-  If the lobby tells the client they're kicked, close out the game and the lobby
-*/
-
