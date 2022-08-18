2187d38fae1c1b6bb1d546723426b8aabf67d840
headpatmonster <20297536+headpatmonster@users.noreply.github.com>
2022-08-17 22:22:52 -0500

    Add graphics to Rooms

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite, Graphics, Container } from 'pixi.js';
+import { Application, Container } from 'pixi.js';
 //import anime from 'animejs';
 
 //-----------------------------------------------------------
@@ -19,6 +19,10 @@ function connectResizeFunction(fx) {
   fx();
 }
 
+function removeResizeFunction(fx) {
+  window.removeEventListener('resize', fx);
+}
+
 //-----------------------------------------------------------
 // Game
 
@@ -26,25 +30,6 @@ let gameContainer = new Container();
 app.stage.addChild(gameContainer);
 
 // Background
-
-let backgroundColor = new Graphics();
-gameContainer.addChild(backgroundColor);
-
-let blob = Sprite.from('./assets/letters/whiteBlob.png');
-backgroundColor.addChild(blob);
-blob.tint = 0xffe29c;
-
-connectResizeFunction(() => {
-  if (window.innerWidth / window.innerHeight > 1) {
-    blob.height = window.innerHeight * 1.1;
-    blob.width = blob.height;
-  } else {
-    blob.width = window.innerWidth * 1.1;
-    blob.height = blob.width;
-  }
-  blob.x = window.innerWidth / 2 - blob.width / 2;
-  blob.y = window.innerHeight / 2 - blob.height / 2;
-});
 
 // Console logging userID
 // socket.on('tellID', (userID) => {
@@ -57,14 +42,9 @@ connectResizeFunction(() => {
 // Resize stuffs
 
 // Listen for window resize events
-function resize() {
-	app.renderer.resize(window.innerWidth, window.innerHeight);
-
-  // Redraw the background color
-  backgroundColor.clear().beginFill(0xffd369).drawRect(0, 0, window.innerWidth, window.innerHeight);
-}
-window.addEventListener('resize', resize);
-resize();
+connectResizeFunction(() => {
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+});
 
 /*
   Start by having a "waiting for lobby" text on the screen
@@ -74,3 +54,8 @@ resize();
   If the lobby tells the client they're kicked, close out the game and the lobby
 */
 
+export {
+  app,
+  connectResizeFunction,
+  removeResizeFunction
+}
--- src/client/objects/Room.ts
+++ src/client/objects/Room.ts
@@ -1,3 +1,5 @@
+import { Container, Graphics, Sprite } from "pixi.js";
+import { app, connectResizeFunction } from "../app";
 import { GameCode } from "../GameDirectory";
 import { BaseGame } from "./BaseGame";
 import { Player } from "./Player";
@@ -7,13 +9,44 @@ export class Room {
   players: [Player?];
 
   gameCode: GameCode;
-
   game?: BaseGame;
+
+  roomContainer: Container;
+  backgroundColorGraphics: Graphics;
+  blobSprite: Sprite;
   
   constructor(ID: string, players: [Player?], gameCode: GameCode) {
     this.ID = ID;
     this.players = players;
     this.gameCode = gameCode;
+
+    // Room graphics
+    this.roomContainer = new Container();
+    app.stage.addChild(this.roomContainer);
+
+    this.backgroundColorGraphics = new Graphics();
+    this.roomContainer.addChild(this.backgroundColorGraphics);
+
+    this.blobSprite = Sprite.from('./assets/letters/whiteBlob.png');
+    this.backgroundColorGraphics.addChild(this.blobSprite);
+    this.blobSprite.tint = 0xffe29c;
+
+    // Background
+    connectResizeFunction(this.resize);
+  }
+
+  resize() {
+    this.backgroundColorGraphics.clear().beginFill(0xffd369).drawRect(0, 0, window.innerWidth, window.innerHeight);
+
+    if (window.innerWidth / window.innerHeight > 1) {
+      this.blobSprite.height = window.innerHeight * 1.1;
+      this.blobSprite.width = this.blobSprite.height;
+    } else {
+      this.blobSprite.width = window.innerWidth * 1.1;
+      this.blobSprite.height = this.blobSprite.width;
+    }
+    this.blobSprite.x = window.innerWidth / 2 - this.blobSprite.width / 2;
+    this.blobSprite.y = window.innerHeight / 2 - this.blobSprite.height / 2;
   }
 
   addPlayer(player: Player) {
