9da4ec2c0d7f1e8a4fffd871ff42a0754779de9a
headpatmonster <20297536+headpatmonster@users.noreply.github.com>
2022-08-18 00:41:11 -0500

    Update socket.js to TypeScript

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
 // By including this line, webpack adds the CSS style to our HTML page
 import scss from './sass/style.scss';
 
-import './utils/socket.js';
+import './utils/socket.ts';
 import './app.ts';
