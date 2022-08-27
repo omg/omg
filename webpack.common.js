8eabb7d9306fe9f474540962eb8adb51fe228a2b
headpatmonster <krysppy@gmail.com>
2022-08-27 14:34:42 -0500

    Add font loading to webpack

--- webpack.common.js
+++ webpack.common.js
@@ -34,8 +34,20 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.s?css$/i,
+        test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ],
   },
