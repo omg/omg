fc08d994e5183a5eedb996f114fe1f8fde940e3c
headpatmonster <krysppy@gmail.com>
2022-09-20 19:07:02 -0500

    add contructor to team

--- src/server/entities/Entity.ts
+++ src/server/entities/Entity.ts
@@ -1,5 +1,8 @@
 import { Player } from "./Player";
 
+// might need a is(Entity) method in Entity
+// to test if a Player is part of a Team
+
 export interface Entity {
   name: string;
   color: number;
--- src/server/entities/Team.ts
+++ src/server/entities/Team.ts
@@ -3,9 +3,16 @@ import { Player } from "./Player";
 
 export class Team implements Entity {
   name: string;
-  color: number;
   canTeamChat: boolean;
+  color: number;
   players: Player[];
+
+  constructor(name: string, canTeamChat: boolean, color: number = 0x2afa23) {
+    this.name = name;
+    this.canTeamChat = canTeamChat;
+    this.color = color;
+    this.players = [];
+  }
 
   getPlayers(): Player[] {
     return this.players;
