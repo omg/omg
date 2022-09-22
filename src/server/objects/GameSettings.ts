76d1653640d95f296ba528973b4fa0fbb8954b3f
headpatmonster <krysppy@gmail.com>
2022-09-22 04:48:13 -0500

    Remove teams from GameSettings

--- src/server/objects/GameSettings.ts
+++ src/server/objects/GameSettings.ts
@@ -1,37 +1,20 @@
-import { Player } from "../entities/Player";
-import { Team } from "../entities/Team";
+import { GameCode } from "../GameDirectory";
 
-export class GameSettings {
-  //game: typeof Game;
-  // Game can be in the GameHandler - is not necessary to be in GameSettings
-  // since the game doesn't care about this information - keep GameSettings
-  // about variables the game may need which can be modified by the room
-
+// could upgrade to a class later - but right now no special methods are required
+export type GameSettings = {
   // TODO let the game decide if the teams or minPlayers should be modifiable
   // TODO where do maxPlayers fit? gamesettings? room?
-  // TODO spectators?
+  // one of the prior AND lobby?
+
+  gameCode: GameCode;
   
   minPlayers: number;
-  teams: Team[];
+
+  //teams: Team[]; ?? TODO Where do teams go????
+
   //mods: Mod[];
+  //gameOptions: any; ?? TODO
   //teamBalancing: boolean;
   //minPlayers: number;
   //map from Crashgrid
-
-  private checkTeams() {
-    if (this.teams.length === 0) this.teams.push(new Team("Players", false));
-  }
-
-  constructor(minPlayers: number, teams: Team[]) {
-    this.minPlayers = minPlayers;
-    this.teams = teams;
-    this.checkTeams();
-  }
-
-  getTeam(player: Player) {
-    for (let team of this.teams) {
-      if (team.hasPlayer(player)) return team;
-    }
-    return null;
-  }
 }
