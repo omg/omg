c01a823f26727f0fca07e4438f18a96491334b37
headpatmonster <krysppy@gmail.com>
2022-09-22 04:43:09 -0500

    Add Crashgrid features to Team

--- src/server/entities/Team.ts
+++ src/server/entities/Team.ts
@@ -4,14 +4,22 @@ import { Player } from "./Player";
 export class Team implements Entity {
   name: string;
   canTeamChat: boolean;
+  hiddenPlayers: boolean;
+  balancePercentage: number;
   color: number;
-  players: Player[];
 
-  constructor(name: string, canTeamChat: boolean, color: number = 0x2afa23) {
+  players: Player[];
+  queue: Player[];
+
+  constructor(name: string, canTeamChat: boolean, hiddenPlayers: boolean = false, balancePercentage: number = 1, color: number = 0x2afa23) {
     this.name = name;
     this.canTeamChat = canTeamChat;
+    this.hiddenPlayers = hiddenPlayers;
+    this.balancePercentage = balancePercentage;
     this.color = color;
+
     this.players = [];
+    this.queue = [];
   }
 
   getPlayers(): Player[] {
@@ -20,5 +28,14 @@ export class Team implements Entity {
 
   hasPlayer(player: Player) {
     return this.players.includes(player);
+  }
+
+  getJSON() {
+    return {
+      name: this.name,
+      color: this.color,
+      players: this.hiddenPlayers ? -1 : this.players.length,
+      queue: this.hiddenPlayers ? -1 : this.queue.length
+    }
   }
 }
